2ef7891f37b7905876c930d86b3c46a5b8fddf28
Daniel Loy <the.danielloy@gmail.com>
2013-12-09 13:03:58 -0800

    first attempt

---
+++ src/cart/views.py
@@ -0,0 +1,67 @@
+
+from django.shortcuts import render_to_response, HttpResponse, HttpResponseRedirect, RequestContext, Http404
+
+from products.models import Product
+from .models import Cart, CartItem
+from .forms import ProductQtyForm
+
+
+def add_to_cart(request):
+    request.session.set_expiry(0)
+    try:
+        cart_id  = request.session['cart_id']
+    except:
+        cart = Cart()
+        cart.save()
+        request.session['cart_id'] = cart.id
+        cart_id = cart.id
+
+    if request.method == "POST":
+        form = ProductQtyForm(request.POST)
+        if form.is_valid():
+            product_slug = form.cleaned_data['slug']
+            product_quantity = form.cleaned_data['quantity']
+            try:
+                product = Product.objects.get(slug=product_slug)
+            except Exception:
+                product = None
+            
+            try:
+                cart = Cart.objects.get(id=cart_id)
+            except Exception:
+                cart = None
+            new_cart, created = CartItem.objects.get_or_create(cart=cart, product=product)
+            if product_quantity > 0:    
+                new_cart.quantity = product_quantity
+                new_cart.total = int(new_cart.quantity)* new_cart.product.price
+                new_cart.save()
+            else:
+                pass
+            if created:
+                print 'Created!'
+            print new_cart.product, new_cart.quantity, new_cart.cart
+            return HttpResponseRedirect('/cart/')
+        return HttpResponseRedirect('/contact/')
+    else:
+        raise Http404
+    
+    
+def view(request):
+    try:
+        cart_id = request.session['cart_id']
+        cart = Cart.objects.get(id=cart_id)
+    except :
+        cart = False
+        
+    if cart == False or cart.active == False:
+        print 'Your cart is empty!'
+        
+    if cart and cart.active:
+        cart = cart
+        cart.total = 0
+        for item in cart.cartitem_set.all():
+            cart.total += item.total
+            cart.save()
+
+    request.session['cart_items'] = len(cart.cartitem_set.all())
+    return render_to_response('cart/view.html', locals(), context_instance = RequestContext(request))
---
+++ src/ecommerce/settings/local.py
@@ -0,0 +1,82 @@
+# Django settings for ecommerce project.
+import os 
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'ecommmerce.sqlite',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
+
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))),'static','media')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = '/media/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))),'static','static-only')
+
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))),'static','static'),
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+
+
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))),'static','templates'),
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Uncomment the next line to enable the admin:
+    'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    'django.contrib.admindocs',
+    'south',
+    'products',
+    'contact',
+    'cart',
+)
